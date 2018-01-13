c289cac17d4ec6106173de091d3e04e34dd60e2f
gits2501 <gits2501@users.noreply.github.com>
2018-01-13 20:36:42 +0100

    setting twtRequest content-lengt to value got from client request header

--- server/twiz-server.js
+++ server/twiz-server.js
@@ -442,7 +442,9 @@ console.log(new hmacSha1('base64').digest(key, baseStr));
             console.log('request.content-type: ', this.request.headers['content-type']);
             console.log('request.content-length: ', this.request.headers['content-length']);
             console.log('request.body: ', vault.body);
+            console.log('proxyRequest.headers:', proxyRequest.headers);
             proxyRequest.setHeader('Content-Type', this.request.headers['content-type']); // set type that came
+            proxyRequest.setHeader('Content-Length', this.request.headers['content-length']);
             proxyRequest.write(vault.body); // on api request send body if exists
         }
         proxyRequest.on('error', function(err){
