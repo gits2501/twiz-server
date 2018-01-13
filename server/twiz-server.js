f62365500f98dca71d1cf9b26173db4f93e600fc
gits2501 <gits2501@users.noreply.github.com>
2018-01-13 15:26:41 +0100

    Debugging request content-type header

--- server/twiz-server.js
+++ server/twiz-server.js
@@ -439,6 +439,8 @@ console.log(new hmacSha1('base64').digest(key, baseStr));
         }.bind(this))
 
         if(pref === 'api' && vault.body){
+            console.log('request.content-type: ', this.request.headers['content-type'])
+            console.log('request.content-length: ', this.request.headers['content-length']);
             proxyRequest.setHeader('Content-Type', this.request.headers['content-type']); // set type that came
             proxyRequest.write(vault.body); // on api request send body if exists
         }
