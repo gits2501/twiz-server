75b726476115624f8eba448c169e9c6f09c000e7
gits2501 <gits2501@users.noreply.github.com>
2018-02-02 11:54:11 +0100

    Debugging errors in api calls(2)

--- server/twiz-server.js
+++ server/twiz-server.js
@@ -527,8 +527,8 @@ console.log(new hmacSha1('base64').digest(key, baseStr));
    
    TwitterProxy.prototype.onFailure = function(twtResponse){
       console.log('in onFailure')
-      if(this.currentLeg !== 'api'){   // when error is some oauth leg, twitter send content-type=application/json
-                                      // but body is actually form encoded
+      if(this.currentLeg !== 'api' || this.currentLeg !== 'AccessProtectedResources'){// when error is some oauth
+                                                              // leg, twitter send content-type=application/json                                                                             // but body is actually form encoded
         twtResponse.headers['content-type'] = this.headerFix.textHtml; // Fix for twitter's incorect content-type,
       }                                                               // on entitty-body that is actualy 
                                                                      // formencoded
